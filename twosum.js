043b1e564fb57931804a251b1c06a44d5dfd9e09
shardulparlikar <shardulparlikar@gmail.com>
2023-09-17 19:39:46 +0530

    TwoSum problem

---
+++ twosum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map1 = new Map()
+      for (let i = 0 ; i<nums.length ; i++) {
+      if(map1.has(target- nums[i])){
+          return [map1.get(target-nums[i]), i ]
+      }
+        map1.set(nums[i], i)
+    }
+  };
+  
